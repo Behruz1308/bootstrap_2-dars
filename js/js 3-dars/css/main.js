9f4871620ecc452e8893cbecf39aa10ffbefe5fc
Behruz <xujamurodovbehruz@gmail.com>
2023-05-20 08:04:45 +0500

    bootstrap 2-dars
    bootstrap 2-dars kodlari yuklandi!

---
+++ js/js 3-dars/css/main.js
@@ -0,0 +1,35 @@
+/* JS data tupes: object */
+/*
+let arrayObject  = [45,78,'salom','matn',true,false];
+console.log(arrayObject[2]);
+*/
+/*let oddiyObject ={ism: "Oloviddin", yoshi:14};
+let talabami ="yoq talabamas";
+if (oddiyObject.yoshi > 17){
+    talabami = "Ha,talaba!"
+}
+console.log("Oloviddin talabami?" + talabami);*/
+/*let oddiyObject ={ism: "komoliddin", yoshi:12};> 1
+let talabami ="yoq talabaemas u maktab oquvchisi!";
+if (oddiyObject.yoshi 2){
+    talabami = "Ha u maktab oquvchisi!";
+}
+console.log("komoliddin talabami?" + talabami);*/
+/* data in JS */
+let sana = new Date();
+
+let yil = sana.getFullYear();
+let oy = sana.getMonth();
+let kun = sana.getDate();
+let haftakuni = sana.getDay();
+let oylar =["Yanvar","Fevral","Mart","Aprel","May","Iyun","Iyul","Avgust","Sentabr","oktabr","Noyabr","Dekabr"];
+
+document.write("Bugun "+yil+"-yil "+oy+"-oyning "+kun"-kuni. Haftaning esa " +haftakuni+"-kuni.");
+
+
+
+
+
+
+
+
